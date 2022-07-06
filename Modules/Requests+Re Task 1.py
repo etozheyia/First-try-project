4a78d712e37613c68b2de528a8d95e8e09401bd9
Er Sv <70100973+etozheyia@users.noreply.github.com>
2022-07-06 17:59:19 +0300

    Update master

--- Modules/Requests+Re Task 1.py
+++ Modules/Requests+Re Task 1.py
@@ -5,7 +5,10 @@ import requests
 import re
 
 url = 'https://www.summet.com/dmsi/html/codesamples/addresses.html'
-response = requests.get(url, timeout=5)
+try:
+    response = requests.get(url, timeout=5)
+except TimeoutError:
+    print('Превышено время ожидания запроса')
 text = response.text
 result = re.findall(r'\(\d{3}\)\s\d{3}-\d{4}', text)
 print(result)
--- Modules/Requests+Re Task 2.py
+++ Modules/Requests+Re Task 2.py
@@ -5,7 +5,10 @@ import requests
 import re
 
 url = 'https://proglib.io/'
-response = requests.get(url, timeout=5)
+try:
+    response = requests.get(url, timeout=5)
+except TimeoutError:
+    print('Превышено время ожидания запроса')
 content = response.text
 regex_without_format = r'http[:\w./_-]*\.'
 result = re.findall(rf'{regex_without_format}png|{regex_without_format}jpg', content)
